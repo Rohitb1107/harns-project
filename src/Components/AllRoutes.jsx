eb8ee09823a623e0dcef1d06ad121b5a31bf024c
Rohit <stylishrb711@gmail.com>
2022-09-14 12:17:34 +0530

    data update!

--- src/Components/AllRoutes.jsx
+++ src/Components/AllRoutes.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Home from "./Home";
 import Navbar from "./Navbar";
+import Register from "./Register";
+import { Routes, Route } from "react-router-dom";
 
 const AllRoutes = () => {
   return (
     <div>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
     </div>
   );
 };
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [data, setData] = useState([]);
 
-export default Register
+  function handleInput(e) {
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8080/Users", data)
+      .then((res) => {
+        alert("User added successfully!");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="form-div w-50">
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          onChange={handleInput}
+        />
+        <input
+          type="text"
+          placeholder="Gender"
+          name="gender"
+          onChange={handleInput}
+        />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
