22878bcdb720eacc293f1525389e699266f99fff
Rohit <stylishrb711@gmail.com>
2022-09-14 10:35:03 +0530

    Navbar done!

---
+++ src/Components/AllRoutes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./Home";
+import Navbar from "./Navbar";
+
+const AllRoutes = () => {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
+};
+
+export default AllRoutes;
